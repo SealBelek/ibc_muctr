71bdd03ae98c863f6e9ec5c2b7b46826fe1fc8fb
Vitalii <36367626+SealBelek@users.noreply.github.com>
2020-09-07 23:30:20 +0300

    Update download.py

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import os
 
 if len(sys.argv) < 4+1:
   exit()
@@ -14,8 +15,10 @@ data={"family":login, "library_card_number":password}
 url = "https://lib.muctr.ru/reader/login"
 r = s.post(url, data)
 
-for i in range(1, pages):
+for i in range(1, int(pages)):
   response = s.get(bookUrl + f'{i}')
   file = open(f"{i}.png", "wb")
-    while(file.write(response.content) == 0):
-      pass
+  while(file.write(response.content) == 0):
+    pass
+    
+os.system('files=$(ls -1v *.png); convert $files book.pdf')
